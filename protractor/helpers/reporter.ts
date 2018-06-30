26cde60a1260ddb4a3c20f0344eef84c20dd7eb1
Alejandro Franco <afranco7@gmail.com>
2018-06-30 13:24:17 -0500

    Reporter typescript migration

---
+++ protractor/helpers/reporter.ts
@@ -0,0 +1,7 @@
+﻿import { DisplayProcessor, SpecReporter } from "jasmine-spec-reporter";
+
+export let reporter = () => {
+    jasmine.getEnv().addReporter(new SpecReporter({
+        customProcessors: [DisplayProcessor],
+    }));
+};
